2199d4c71998b0f13d1c040ab281210bc7f70867
bakir <bakir.supergirl@gmail.com>
2024-10-16 18:13:48 +0600

    fix-worker

--- backend/main/asgi.py
+++ backend/main/asgi.py
@@ -1,19 +1,7 @@
-"""
-ASGI main for main project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
-import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-django.setup()
 
 from my_app.routing import websocket_urlpatterns
 
--- backend/my_app/urls.py
+++ backend/my_app/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ProductViewSet, CategoryViewSet, OrderViewSet
+from .views import UserProfileViewSet, UserDetailViewSet, UserGroupViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'products', ProductViewSet)
-router.register(r'categories', CategoryViewSet)
-router.register(r'orders', OrderViewSet)
+router.register(r'users', UserProfileViewSet)
+router.register(r'userdetails', UserDetailViewSet)
+router.register(r'usergroups', UserGroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
