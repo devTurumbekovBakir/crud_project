059ccde6aaa63778a1a847640281df5c985f1953
bakir <bakir.supergirl@gmail.com>
2024-10-16 19:05:20 +0600

    fix-admin.site

--- backend/my_app/admin.py
+++ backend/my_app/admin.py
@@ -1,8 +1,7 @@
-# from django.contrib import admin
-#
-# from .models import User, Category, Product, Profile
-#
-# admin.site.register(User)
-# admin.site.register(Category)
-# admin.site.register(Product)
-# admin.site.register(Profile)
+from django.contrib import admin
+
+from .models import UserProfile, UserDetail, UserGroup
+
+admin.site.register(UserProfile)
+admin.site.register(UserDetail)
+admin.site.register(UserGroup)
